d255b019d19119f9751dd3811b1937811dc80c00
abrmv23 <aaabramov_10@edu.hse.ru>
2025-10-19 19:28:34 +0300

    Создание файла main.py простейшего кода

---
+++ main.py
@@ -0,0 +1,20 @@
+
+def plus(a,b):
+    return a+b
+
+def minus(a,b):
+    return a-b
+
+def times(a,b):
+    return a*b
+
+def divided(a,b):
+    if b == 0:
+        return "Операция недействительна"
+    else:
+        return a/b
+    
+def main():
+    return
+
+main()
